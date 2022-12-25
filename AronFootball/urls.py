97b2a7cb39685adb08b1ba4b3107e174f26ad127
siavash hasanpoor <siavash.hasanpoor@gmail.com>
2022-12-26 00:41:24 +0330

    Initial Project

---
+++ AronFootball/urls.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("aron.urls", namespace="aron")),
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {
+            "document_root": settings.MEDIA_ROOT,
+        },
+    ),
+]
---
+++ aron/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AronConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "aron"
