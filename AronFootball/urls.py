045df1a63b4f3251161769396de6b0bdddf3a7ef
siavash hasanpoor <siavash.hasanpoor@gmail.com>
2023-01-08 09:19:22 +0330

    update url

--- AronFootball/urls.py
+++ AronFootball/urls.py
@@ -10,14 +10,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("aron.urls", namespace="aron")),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += staticfiles_urlpatterns()
 urlpatterns += [
-    re_path(
-        r"^media/(?P<path>.*)$",
-        serve,
-        {
-            "document_root": settings.MEDIA_ROOT,
-        },
-    ),
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
 ]
